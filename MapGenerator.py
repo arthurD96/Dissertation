ab7bf23d84c625d61b2ff33b68c695d5de654b1c
ArthurD <arthurdavison@hotmail.co.uk>
2019-02-22 14:13:27 +0000

    Scatter Graph Map
    Generated Map is now a list of xy co-ordinates which gets displayed on a matplotlib scatter graph

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,19 +1,48 @@
 import random
+import matplotlib.pyplot as plt
+
+
+def main():
+    numberOfCities = 100
+    maxDistance = 1000
+    map = generateMap(numberOfCities, maxDistance)
+    print(map)
 
 
 def generateMap(numberOfCities, maxDistance):
-    tspMap = []
+    xCord = [random.randrange(1, maxDistance + 1, 1) for _ in range(numberOfCities)]
+    yCord = [random.randrange(1, maxDistance + 1, 1) for _ in range(numberOfCities)]
+    n = list(range(1, numberOfCities + 1))
+    xyCord = list(zip(xCord, yCord))
+    duplicates = True
+    while duplicates:
+        if len(xyCord) == len(set(xyCord)):
+            duplicates = False
+        else:
+            s = set()
+            any(x in s or s.add(x) for x in xyCord)
+            s = set()
+            duplicates1 = set(x for x in xyCord if x in s or s.add(x))
+            duplicates1 = list(duplicates1)
+            for i in range(len(duplicates1)):
+                newX = duplicates1[i][0]
+                newY = duplicates1[i][1]
+                if newX == maxDistance and newY == maxDistance:
+                    newX = 0
+                    newY = 0
+                elif newX == maxDistance:
+                    newY += 1
+                else:
+                    newX += 1
+                newXy = (newX, newY)
+                xyCord.remove(duplicates1[i])
+                xyCord.append(newXy)
+    unzippedXy = list(zip(*xyCord))
+    xCord = unzippedXy[0]
+    yCord = unzippedXy[1]
+    plt.scatter(xCord, yCord)
+    plt.show()
+    return xyCord
 
-    for i in range(numberOfCities):
-        tspDistances = []
-        for j in range(numberOfCities):
-            tspDistances.append('')
-        tspMap.append(tspDistances)
-    for i in range(0, numberOfCities):
-        for j in range(0, numberOfCities):
-            if i == j:
-                tspMap[i][j] = 0
-            elif tspMap[i][j] == '':
-                distance = random.randint(1, maxDistance)
-                tspMap[i][j] = distance
-                tspMap[j][i] = distance
+
+main()
