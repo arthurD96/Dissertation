ea8ca3c18e3d49f32388df2a91f7778addd3b94c
ArthurD <arthurdavison@hotmail.co.uk>
2019-02-23 12:49:43 +0000

    Fitness Function Added
    Fitness functions for path and binary data added

---
+++ CalculateFitness.py
@@ -0,0 +1,29 @@
+import math
+
+
+def calculateFitnessBinary(population, cityCords):
+    populationWithFitness = []
+    for solution in population:
+        fitness = 0
+        for i in range(1, len(solution)):
+            cityTo = int(solution[i], 2)
+            cityFrom = int(solution[i - 1], 2)
+            distanceX = abs(cityCords[cityTo - 1][0] - cityCords[cityFrom - 1][0])
+            distanceY = abs(cityCords[cityTo - 1][1] - cityCords[cityFrom - 1][1])
+            fitness += math.sqrt((distanceX ** 2) + (distanceY ** 2))
+        populationWithFitness.append([solution, fitness])
+    return populationWithFitness
+
+
+def calculateFitnessPath(population, cityCords):
+    populationWithFitness = []
+    for solution in population:
+        fitness = 0
+        for i in range(1, len(solution)):
+            cityTo = int(solution[i])
+            cityFrom = int(solution[i - 1])
+            distanceX = abs(cityCords[cityTo - 1][0] - cityCords[cityFrom - 1][0])
+            distanceY = abs(cityCords[cityTo - 1][1] - cityCords[cityFrom - 1][1])
+            fitness += math.sqrt((distanceX ** 2) + (distanceY ** 2))
+        populationWithFitness.append([solution, fitness])
+    return populationWithFitness
--- MapGenerator.py
+++ MapGenerator.py
@@ -1,28 +1,19 @@
 import random
-import matplotlib.pyplot as plt
-
-
-def main():
-    numberOfCities = 100
-    maxDistance = 1000
-    map = generateMap(numberOfCities, maxDistance)
-    print(map)
 
 
 def generateMap(numberOfCities, maxDistance):
     xCord = [random.randrange(1, maxDistance + 1, 1) for _ in range(numberOfCities)]
     yCord = [random.randrange(1, maxDistance + 1, 1) for _ in range(numberOfCities)]
-    n = list(range(1, numberOfCities + 1))
-    xyCord = list(zip(xCord, yCord))
+    xyCords = list(zip(xCord, yCord))
     duplicates = True
     while duplicates:
-        if len(xyCord) == len(set(xyCord)):
+        if len(xyCords) == len(set(xyCords)):
             duplicates = False
         else:
             s = set()
-            any(x in s or s.add(x) for x in xyCord)
+            any(x in s or s.add(x) for x in xyCords)
             s = set()
-            duplicates1 = set(x for x in xyCord if x in s or s.add(x))
+            duplicates1 = set(x for x in xyCords if x in s or s.add(x))
             duplicates1 = list(duplicates1)
             for i in range(len(duplicates1)):
                 newX = duplicates1[i][0]
@@ -35,14 +26,8 @@ def generateMap(numberOfCities, maxDistance):
                 else:
                     newX += 1
                 newXy = (newX, newY)
-                xyCord.remove(duplicates1[i])
-                xyCord.append(newXy)
-    unzippedXy = list(zip(*xyCord))
-    xCord = unzippedXy[0]
-    yCord = unzippedXy[1]
-    plt.scatter(xCord, yCord)
-    plt.show()
-    return xyCord
+                xyCords.remove(duplicates1[i])
+                xyCords.append(newXy)
 
+    return xyCords
 
-main()
