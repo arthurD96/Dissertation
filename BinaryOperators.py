64b376e7daec7042215860e0fa36db1e704353da
ArthurD <arthurdavison@hotmail.co.uk>
2019-03-06 16:34:01 +0000

    Binary Operators Merged
    All binary mutations and crossovers added to one class and pytests updated accordingly

---
+++ BinaryOperators.py
@@ -0,0 +1,74 @@
+import random
+
+
+def runMutation(population, mutationProbability):
+    mutatedPopulation = []
+
+    for i in range(0, len(population)):
+        currentSolution = population[i]
+        mutatedSolution = []
+
+        for allele in currentSolution:
+            mutatedAllele = ""
+            for bit in allele:
+                if random.random() < mutationProbability:
+                    if bit == "0":
+                        bit = "1"
+                        mutatedAllele = mutatedAllele + bit
+                    else:
+                        bit = "0"
+                        mutatedAllele = mutatedAllele + bit
+                else:
+                    mutatedAllele = mutatedAllele + bit
+
+            mutatedSolution.append(mutatedAllele)
+        mutatedPopulation.append(mutatedSolution)
+
+    return mutatedPopulation
+
+
+def runCrossover(population):
+    childPopulation = []
+    numberOfBits = len(population[0][0])
+
+    for i in range(0, len(population), 2):
+        try:
+            parentOne = ''.join(population[i])
+            parentTwo = ''.join(population[i + 1])
+        except IndexError:
+            parentOne = ''.join(population[i])
+            parentTwo = ''.join(population[0])
+
+        crossoverBit = random.randint(1, len(parentOne) - 1)
+
+        childOne = parentOne[:crossoverBit] + parentTwo[crossoverBit:]
+        childTwo = parentTwo[:crossoverBit] + parentOne[crossoverBit:]
+        children = [childOne, childTwo]
+
+        for string in children:
+            child = [string[i:i + numberOfBits] for i in range(0, len(string), numberOfBits)]
+            childPopulation.append(child)
+
+    return childPopulation
+
+
+def runRepair(population):
+    numberOfCities = len(population[0])
+    binaryLength = len("{0:b}".format(numberOfCities))
+    validPopulation = []
+    exampleSolution = []
+
+    for j in range(1, numberOfCities + 1):
+        city = bin(j)[2:].zfill(binaryLength)
+        exampleSolution.append(city)
+    setExample = frozenset(exampleSolution)
+
+    for solution in population:
+        validCities = [x for x in solution if x in setExample]
+        validCities = list(dict.fromkeys(validCities))
+        missingCities = [x for x in setExample if x not in solution]
+        missingCities = list(missingCities)
+        validCities.extend(missingCities)
+        validPopulation.append(validCities)
+
+    return validPopulation
