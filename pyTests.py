e8308c4887f0296f2ab502ccb3f27f39221cd388
ArthurD <arthurdavison@hotmail.co.uk>
2019-03-11 18:01:23 +0000

    Selection Operators Merged
    Tournament and Roulette selection added to one class and all references updated including merger of existing pytests

--- pyTests.py
+++ pyTests.py
@@ -1,8 +1,11 @@
 import BinaryOperators
-import pytest
+import Selection
 
 population = [['010', '100', '101', '001', '011'], ['100', '011', '010', '001', '101'],
               ['101', '010', '001', '100', '011'], ['011', '001', '101', '010', '100']]
+
+populationWithFitness = [[['010', '100', '101', '001', '011'], 10], [['100', '011', '010', '001', '101'], 12],
+                         [['101', '010', '001', '100', '011'], 14], [['011', '001', '101', '010', '100'], 16]]
 
 
 def testClassicalCrossoverPopulationSize():
@@ -45,21 +48,11 @@ def testRepairBinaryOnNonValidSolution():
     correctPopulation = [['001', '010', '011', '101', '100']]
     assert testPopulation == correctPopulation
 
-import pytest
-import TournamentSelection
-import RouletteSelection
-
-populationWithFitness = [[['010', '100', '101', '001', '011'], 10], [['100', '011', '010', '001', '101'], 12],
-                         [['101', '010', '001', '100', '011'], 14], [['011', '001', '101', '010', '100'], 16]]
-
-population = [['010', '100', '101', '001', '011'], ['100', '011', '010', '001', '101'],
-              ['101', '010', '001', '100', '011'], ['011', '001', '101', '010', '100']]
-
 
 def testWinningPopulationSizeTournamentSelection():
     selectionPercentage = 50
     winningPopulationPercentage = 25
-    testPopulation = TournamentSelection.tournamentSelection(populationWithFitness, selectionPercentage,
+    testPopulation = Selection.tournamentSelection(populationWithFitness, selectionPercentage,
                                                              winningPopulationPercentage)
     assert len(testPopulation) == 1
 
@@ -67,26 +60,27 @@ def testWinningPopulationSizeTournamentSelection():
 def testBestPopulationReturnedTournamentSelection():
     selectionPercentage = 100
     winningPopulationPercentage = 25
-    testPopulation = TournamentSelection.tournamentSelection(populationWithFitness, selectionPercentage,
+    testPopulation = Selection.tournamentSelection(populationWithFitness, selectionPercentage,
                                                              winningPopulationPercentage)
     assert testPopulation[0] == ['010', '100', '101', '001', '011']
 
 
 def testWinningPopulationSizeRouletteSelection():
     winninPopulationPercentage = 50
-    testPopulation = RouletteSelection.rouletteSelection(populationWithFitness, winninPopulationPercentage)
+    testPopulation = Selection.rouletteSelection(populationWithFitness, winninPopulationPercentage)
 
     assert len(testPopulation) == 2
 
 
 def testValidPopulationRouletteSelection():
     winninPopulationPercentage = 25
-    testPopulation = RouletteSelection.rouletteSelection(populationWithFitness, winninPopulationPercentage)
+    testPopulation = Selection.rouletteSelection(populationWithFitness, winninPopulationPercentage)
     assert testPopulation[0] in population
 
 
 def testValidPopulationTournamentSelection():
     winninPopulationPercentage = 50
     selectionPercentage = 50
-    testPopulation = TournamentSelection.tournamentSelection(populationWithFitness,selectionPercentage ,winninPopulationPercentage)
+    testPopulation = Selection.tournamentSelection(populationWithFitness, selectionPercentage,
+                                                             winninPopulationPercentage)
     assert testPopulation[1] in population
